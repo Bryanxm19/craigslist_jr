fe4f6659f39c9cda23473b98c9d9c5081c7eea88
Bryan Mateer <email": null>
2017-05-08 14:55:46 -0400

    Create new action for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,10 @@
 class ArticlesController < ApplicationController
+  def new
+    @category = Category.find(params[:category_id])
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:price, :email, :title, :description)
+    end
 end
