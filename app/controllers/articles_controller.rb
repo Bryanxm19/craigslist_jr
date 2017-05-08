a12bdf7efca8ecfc261d14a5dcec763af02f4f3e
Bryan Mateer <email": null>
2017-05-08 16:24:47 -0400

    Add all route actions for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,33 @@
 class ArticlesController < ApplicationController
+  def create
+    @category = Category.find(params[:category_id])
+    @article = @category.articles.create(article_params)
+    redirect_to category_path(@category)
+  end
+
   def new
     @category = Category.find(params[:category_id])
+  end
+
+  def edit
+    @category = Category.find(params[:category_id])
+    @article = Article.find(params[:id])
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update_attributes(article_params)
+    redirect_to category_path(@article.category)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to category_path(@article.category)
   end
 
   private
